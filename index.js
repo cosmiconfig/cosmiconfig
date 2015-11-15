2856c30f7b5f3f515952dec6459662b3e15a249e
Bogdan Chadkin <trysound@yandex.ru>
2015-11-16 02:10:16 +0300

    Index refactoring

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var loadRc = require('./lib/loadRc');
 var loadJs = require('./lib/loadJs');
 var loadDefinedFile = require('./lib/loadDefinedFile');
 
-var DONE = 'done';
 var parsedCliArgs = minimist(process.argv);
 
 module.exports = function(moduleName, options) {
@@ -29,75 +28,35 @@ module.exports = function(moduleName, options) {
   var stopDir = options.stopDir || oshomedir();
   var splitSearchPath = splitPath(options.cwd);
 
-  return new Promise(function(resolve, reject) {
+  if (options.configPath) {
+    return loadDefinedFile(options.configPath, options.format);
+  }
 
-    find();
+  function find() {
+    var currentSearchPath = joinPath(splitSearchPath);
 
-    function find() {
-      if (options.configPath) {
-        loadDefinedFile(options.configPath, options.format).then(function(result) {
-          return finishWith(result);
-        }).catch(reject);
-        return;
+    return Promise.resolve(null).then(function() {
+      if (!options.packageProp) return;
+      return loadPackageProp(currentSearchPath, options.packageProp);
+    }).then(function(result) {
+      if (result || !options.rc) return result;
+      return loadRc(path.join(currentSearchPath, options.rc), {
+        strictJson: options.rcStrictJson,
+      });
+    }).then(function(result) {
+      if (result || !options.js) return result;
+      return loadJs(path.join(currentSearchPath, options.js));
+    }).then(function(result) {
+      if (result) return result;
+      // Notice the mutation of splitSearchPath
+      if (currentSearchPath === stopDir || !splitSearchPath.pop()) {
+        return null;
       }
+      return find();
+    });
+  }
 
-      var currentSearchPath = joinPath(splitSearchPath);
-
-      var search = Promise.resolve(null);
-
-      if (options.packageProp) {
-        search = search.then(function(result) {
-          if (result) return finishWith(result);
-          return loadPackageProp(currentSearchPath, options.packageProp);
-        });
-      }
-
-      if (options.rc) {
-        search = search.then(function(result) {
-          if (result) return finishWith(result);
-          return loadRc(path.join(currentSearchPath, options.rc), {
-            strictJson: options.rcStrictJson,
-          });
-        });
-      }
-
-      if (options.js) {
-        search = search.then(function(result) {
-          if (result) return finishWith(result);
-          return loadJs(path.join(currentSearchPath, options.js));
-        });
-      }
-
-      search.then(function(result) {
-        if (result) return finishWith(result);
-        return moveUpOrGiveUp(currentSearchPath);
-      })
-        .then(function(result) {
-          if (result === DONE) return resolve(null);
-          find();
-        })
-        .catch(function(err) {
-          if (err === DONE) return;
-          reject(err);
-        });
-
-      function moveUpOrGiveUp(searchPath) {
-        return new Promise(function(resolve) {
-          // Notice the mutation of splitSearchPath
-          if (searchPath === stopDir || !splitSearchPath.pop()) {
-            resolve(DONE);
-          } else {
-            resolve(null);
-          }
-        });
-      }
-
-      function finishWith(result) {
-        resolve(result);
-        throw DONE;
-      }
-    }
-  });
+  return find();
 };
 
 function splitPath(x) {
