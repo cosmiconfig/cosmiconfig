78ca8e2abeae75ab2aad2c1f3eb280b7a24d2dcd
Suhas Karanth <sudo.suhas@gmail.com>
2017-09-10 12:03:43 +0530

    test: Add tests for 100% code coverage
    Add tests:
    - `caches`
      - `clearCaches` clears both file and directory cache.
      - Error not thrown when `clearFileCache`/`clearDirectoryCache`/`clearCaches`
        is called with cache disabled.
      - File results are not cached when it is disabled.
    - `failed-files`
      - `null` is returned by `load` function when neither `searchPath` nor
        `configPath` are specified.
    - `index`
      - Default options are passed to `createExplorer` if options are not passed.
      - Options passed in function paramter take precedence over defaults.
      - `--config` argument is used if available.
      - `--config` argument is not used if `options.argv` is `false`.
      - Custom argument flag can be specified and is used if available.
    - `resolveDir`
      - `searchPath` is returned if it is a directory.
      - Parent directiry of `searchPath` is returned if it is a file.
      - A `Promise` is returned if `sync` is false.
      - Error thrown by `is-directory` is propagated in sync and the `Promise` is
        rejected in async.
    
    Code changes for enabling unit tests:
    - `index.js` - Parse arguments with `minimist` in each invocation of the
      function exported by `cosmiconfig`, not just on requiring the module.
    - `createExplorer.js` - Move helper function `resolveDir` into separate file.

---
+++ test/index.test.js
@@ -0,0 +1,115 @@
+'use strict';
+
+// Mock `createExplorer` because we want to check what it is called with.
+jest.mock('../lib/createExplorer');
+// Mock `minimist` and also provide default implementation which returns an
+// empty object. The factory function returns a mock which we can tweak per
+// test basis.
+jest.mock('minimist', () => jest.fn(() => ({})));
+
+const path = require('path');
+const oshomedir = require('os-homedir');
+const cosmiconfig = require('../');
+
+const minimistMock = require('minimist');
+const createExplorerMock = require('../lib/createExplorer');
+
+describe('cosmiconfig', () => {
+  const moduleName = 'foo';
+  const stopDir = oshomedir();
+  const configPath = path.join(__dirname, 'fixtures/foo.json');
+
+  afterEach(() => {
+    // Clean up a mock's usage data between tests
+    jest.clearAllMocks();
+  });
+
+  it('creates explorer with default options if not specified', () => {
+    cosmiconfig(moduleName);
+
+    expect(createExplorerMock).toHaveBeenCalledTimes(1);
+    expect(createExplorerMock).toHaveBeenCalledWith({
+      packageProp: moduleName,
+      rc: `.${moduleName}rc`,
+      js: `${moduleName}.config.js`,
+      argv: 'config',
+      rcStrictJson: false,
+      stopDir,
+      cache: true,
+      sync: false,
+    });
+  });
+
+  it('creates explorer with preference for given options over defaults', () => {
+    cosmiconfig(moduleName, {
+      rc: `.${moduleName}barrc`,
+      js: `${moduleName}bar.config.js`,
+      argv: false,
+      rcStrictJson: true,
+      stopDir: __dirname,
+      cache: false,
+      sync: true,
+    });
+
+    expect(createExplorerMock).toHaveBeenCalledWith({
+      packageProp: moduleName,
+      rc: `.${moduleName}barrc`,
+      js: `${moduleName}bar.config.js`,
+      argv: false,
+      rcStrictJson: true,
+      stopDir: __dirname,
+      cache: false,
+      sync: true,
+    });
+  });
+
+  it('uses the --config flag by default', () => {
+    minimistMock.mockReturnValueOnce({ config: configPath });
+    cosmiconfig(moduleName);
+
+    expect(createExplorerMock).toHaveBeenCalledWith({
+      packageProp: moduleName,
+      rc: `.${moduleName}rc`,
+      js: `${moduleName}.config.js`,
+      argv: 'config',
+      rcStrictJson: false,
+      stopDir,
+      cache: true,
+      sync: false,
+      configPath,
+    });
+  });
+
+  it('does not use the --config flag if options.argv is false', () => {
+    minimistMock.mockReturnValueOnce({ config: configPath });
+    cosmiconfig(moduleName, { argv: false });
+
+    expect(createExplorerMock).toHaveBeenCalledWith({
+      packageProp: moduleName,
+      rc: `.${moduleName}rc`,
+      js: `${moduleName}.config.js`,
+      argv: false,
+      rcStrictJson: false,
+      stopDir,
+      cache: true,
+      sync: false,
+    });
+  });
+
+  it('uses the argv name specified by options for reading configPath', () => {
+    minimistMock.mockReturnValueOnce({ 'foo-config': configPath });
+    cosmiconfig(moduleName, { argv: 'foo-config' });
+
+    expect(createExplorerMock).toHaveBeenCalledWith({
+      packageProp: moduleName,
+      rc: `.${moduleName}rc`,
+      js: `${moduleName}.config.js`,
+      argv: 'foo-config',
+      rcStrictJson: false,
+      stopDir,
+      cache: true,
+      sync: false,
+      configPath,
+    });
+  });
+});
