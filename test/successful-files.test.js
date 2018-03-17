69f628b878a654ba208fde7b12d44887da435ee3
David Clark <dclark@mapbox.com>
2018-03-16 20:56:22 -0700

    Refactor successful-files.test

--- test/successful-files.test.js
+++ test/successful-files.test.js
@@ -4,148 +4,186 @@ const util = require('./util');
 const cosmiconfig = require('../src');
 
 const absolutePath = util.absolutePath;
-const configFileLoader = util.configFileLoader;
-const testFuncsRunner = util.testFuncsRunner;
-const testSyncAndAsync = util.testSyncAndAsync;
 
-function makeFileTest(file, format) {
-  return sync => () => {
-    expect.hasAssertions();
-    return testFuncsRunner(sync, configFileLoader({ sync, format }, file), [
-      result => {
-        expect(result.config).toEqual({
-          foo: true,
-        });
-        expect(result.filepath).toBe(absolutePath(file));
-      },
-    ]);
+describe('loads defined JSON config path', () => {
+  const file = absolutePath('fixtures/foo.json');
+  const checkResult = result => {
+    expect(result.config).toEqual({ foo: true });
+    expect(result.filepath).toBe(file);
   };
-}
 
-describe('cosmiconfig', () => {
-  describe('load from file', () => {
-    describe('format not specified', () => {
-      testSyncAndAsync(
-        'loads defined JSON config path',
-        makeFileTest('fixtures/foo.json')
-      );
+  test('async', () => {
+    return cosmiconfig()
+      .load(file)
+      .then(checkResult);
+  });
 
-      testSyncAndAsync(
-        'loads defined YAML config path',
-        makeFileTest('fixtures/foo.yaml')
-      );
+  test('sync', () => {
+    const result = cosmiconfig(null, { sync: true }).load(file);
+    checkResult(result);
+  });
+});
 
-      testSyncAndAsync(
-        'loads defined JS config path',
-        makeFileTest('fixtures/foo.js')
-      );
+describe('loads defined YAML config path', () => {
+  const file = absolutePath('fixtures/foo.yaml');
+  const checkResult = result => {
+    expect(result.config).toEqual({ foo: true });
+    expect(result.filepath).toBe(file);
+  };
 
-      testSyncAndAsync(
-        'loads modularized JS config path',
-        makeFileTest('fixtures/foo-module.js')
-      );
+  test('async', () => {
+    return cosmiconfig()
+      .load(file)
+      .then(checkResult);
+  });
 
-      testSyncAndAsync(
-        'loads yaml-like JS config path',
-        makeFileTest('fixtures/foo-yaml-like.js')
-      );
+  test('sync', () => {
+    const result = cosmiconfig(null, { sync: true }).load(file);
+    checkResult(result);
+  });
+});
+
+describe('loads defined JS config path', () => {
+  const file = absolutePath('fixtures/foo.js');
+  const checkResult = result => {
+    expect(result.config).toEqual({ foo: true });
+    expect(result.filepath).toBe(file);
+  };
+
+  test('async', () => {
+    return cosmiconfig()
+      .load(file)
+      .then(checkResult);
+  });
+
+  test('sync', () => {
+    const result = cosmiconfig(null, { sync: true }).load(file);
+    checkResult(result);
+  });
+});
+
+describe('loads modularized JS config path', () => {
+  const file = absolutePath('fixtures/foo-module.js');
+  const checkResult = result => {
+    expect(result.config).toEqual({ foo: true });
+    expect(result.filepath).toBe(file);
+  };
+
+  test('async', () => {
+    return cosmiconfig()
+      .load(file)
+      .then(checkResult);
+  });
+
+  test('sync', () => {
+    const result = cosmiconfig(null, { sync: true }).load(file);
+    checkResult(result);
+  });
+});
+
+describe('loads yaml-like JS config path', () => {
+  const file = absolutePath('fixtures/foo-yaml-like.js');
+  const checkResult = result => {
+    expect(result.config).toEqual({ foo: true });
+    expect(result.filepath).toBe(file);
+  };
+
+  test('async', () => {
+    return cosmiconfig()
+      .load(file)
+      .then(checkResult);
+  });
+
+  test('sync', () => {
+    const result = cosmiconfig(null, { sync: true }).load(file);
+    checkResult(result);
+  });
+});
+
+describe('respects options.configPath', () => {
+  const configPath = absolutePath('fixtures/foo.json');
+  const checkResult = result => {
+    expect(result.config).toEqual({
+      foo: true,
     });
+    expect(result.filepath).toBe(configPath);
+  };
 
-    describe('format specified', () => {
-      testSyncAndAsync(
-        'loads defined JSON config path',
-        makeFileTest('fixtures/foo.json', 'json')
-      );
+  test('async', () => {
+    return cosmiconfig('foo', { configPath })
+      .load()
+      .then(checkResult);
+  });
 
-      testSyncAndAsync(
-        'loads defined YAML config path',
-        makeFileTest('fixtures/foo.yaml', 'yaml')
-      );
+  test('sync', () => {
+    const result = cosmiconfig('foo', { configPath, sync: true }).load();
+    checkResult(result);
+  });
+});
 
-      testSyncAndAsync(
-        'loads defined JS config path',
-        makeFileTest('fixtures/foo.js', 'js')
-      );
-
-      testSyncAndAsync(
-        'loads modularized JS config path',
-        makeFileTest('fixtures/foo-module.js', 'js')
-      );
-
-      testSyncAndAsync(
-        'loads yaml-like JS config path',
-        makeFileTest('fixtures/foo-yaml-like.js', 'js')
-      );
+describe('loads package prop when configPath is package.json', () => {
+  const configPath = absolutePath('fixtures/package.json');
+  const checkResult = result => {
+    expect(result.config).toEqual({
+      bar: 'baz',
     });
+    expect(result.filepath).toBe(configPath);
+  };
 
-    testSyncAndAsync('respects options.configPath', sync => () => {
-      const configPath = absolutePath('fixtures/foo.json');
-      const explorer = cosmiconfig('foo', { configPath, sync });
-      return testFuncsRunner(sync, explorer.load(), [
-        result => {
-          expect(result.config).toEqual({
-            foo: true,
-          });
-          expect(result.filepath).toBe(configPath);
-        },
-      ]);
-    });
+  test('async', () => {
+    return cosmiconfig('foo', { configPath })
+      .load()
+      .then(checkResult);
+  });
 
-    testSyncAndAsync(
-      'loads package prop when configPath is package.json',
-      sync => () => {
-        const configPath = absolutePath('fixtures/package.json');
-        const explorer = cosmiconfig('foo', { configPath, sync });
-        return testFuncsRunner(sync, explorer.load(), [
-          result => {
-            expect(result.config).toEqual({
-              bar: 'baz',
-            });
-          },
-        ]);
-      }
+  test('sync', () => {
+    const result = cosmiconfig('foo', { configPath, sync: true }).load();
+    checkResult(result);
+  });
+});
+
+describe('runs transform', () => {
+  const configPath = absolutePath('fixtures/foo.json');
+  const transform = result => {
+    result.config.foo = [result.config.foo];
+    return result;
+  };
+  const checkResult = result => {
+    expect(result.config).toEqual({ foo: [true] });
+  };
+
+  test('async', () => {
+    return cosmiconfig(null, { transform })
+      .load(configPath)
+      .then(checkResult);
+  });
+
+  test('sync', () => {
+    const result = cosmiconfig(null, { transform, sync: true }).load(
+      configPath
     );
+    checkResult(result);
+  });
+});
 
-    testSyncAndAsync('runs transform', sync => () => {
-      expect.hasAssertions();
-      return testFuncsRunner(
-        sync,
-        configFileLoader(
-          {
-            sync,
-            transform(result) {
-              result.config.foo = [result.config.foo];
-              return result;
-            },
-          },
-          'fixtures/foo.json'
-        ),
-        [
-          result => {
-            expect(result.config).toEqual({ foo: [true] });
-          },
-        ]
-      );
-    });
+describe('does not swallow transform errors', () => {
+  const configPath = absolutePath('fixtures/foo.json');
+  const transform = () => {
+    throw new Error('These pretzels are making me thirsty!');
+  };
 
-    it('does not swallow transform errors', () => {
-      const load = sync =>
-        configFileLoader(
-          {
-            sync,
-            transform() {
-              throw new Error('These pretzels are making me thirsty!');
-            },
-          },
-          'fixtures/foo.json'
-        );
-
-      expect.assertions(2);
-      expect(() => load(true)).toThrow('These pretzels are making me thirsty!');
-
-      return load(false).catch(err => {
-        expect(err.message).toBe('These pretzels are making me thirsty!');
+  test('async', () => {
+    expect.hasAssertions();
+    return cosmiconfig(null, { transform })
+      .load(configPath)
+      .catch(error => {
+        expect(error.message).toBe('These pretzels are making me thirsty!');
       });
-    });
+  });
+
+  test('sync', () => {
+    expect(() => {
+      cosmiconfig(null, { transform, sync: true }).load(configPath);
+    }).toThrow('These pretzels are making me thirsty!');
   });
 });
