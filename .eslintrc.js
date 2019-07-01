d9865d2c3ea2f633813b884dc9d76c5ae4e32474
Chris Blossom <cbloss@gmail.com>
2019-07-01 14:24:04 -0700

    add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@
 
 // first node 8.x LTS release
 const supportedNodeVersion = '8.9';
+const allExtensions = ['.js', '.jsx', '.ts', '.tsx', '.mjs', '.json'];
 
 module.exports = {
   parser: '@typescript-eslint/parser',
@@ -12,11 +13,14 @@ module.exports = {
   extends: [
     'eslint-config-davidtheclark-node',
     'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
     'plugin:jest/recommended',
     'prettier',
     'prettier/@typescript-eslint',
   ],
-  plugins: ['jest', '@typescript-eslint'],
+  plugins: ['jest', '@typescript-eslint', 'import'],
   rules: {
     'no-var': 'off',
     'prefer-const': 'off',
@@ -43,6 +47,19 @@ module.exports = {
     'node/no-unsupported-features/es-builtins': 'off',
     'node/no-unsupported-features/es-syntax': 'off',
     'node/no-unsupported-features/node-builtins': 'off',
+    // Redundant with import/no-extraneous-dependencies
+    'node/no-extraneous-import': 'off',
+    'node/no-extraneous-require': 'off',
+    // Redundant with import/no-unresolved
+    'node/no-missing-import': 'off',
+    'node/no-missing-require': 'off',
+
+    /**
+     * eslint-plugin-import
+     */
+    'import/no-default-export': 'error',
+    'import/no-named-export': 'off',
+    'import/prefer-default-export': 'off',
 
     /**
      * eslint-plugin-jest
@@ -53,6 +70,21 @@ module.exports = {
     'jest/prefer-todo': 'error',
   },
 
+  settings: {
+    node: {
+      convertPath: {
+        'src/**/*.{js,ts}': ['^src/(.+?)\\.(js|ts)$', 'dist/$1.js'],
+        'src/**/.*.{js,ts}': ['^src/(.+?)\\.(js|ts)$', 'dist/$1.js'],
+      },
+      tryExtensions: allExtensions,
+    },
+    'import/resolver': {
+      node: {
+        extensions: allExtensions,
+      },
+    },
+    'import/extensions': allExtensions,
+  },
   overrides: [
     {
       files: ['*.js', '.*.js'],
