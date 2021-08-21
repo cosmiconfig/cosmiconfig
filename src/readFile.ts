99265fbc9a7b242fbf38759508e7c259e448c06e
Tom Jenkinson <tom@tjenkinson.me>
2021-08-21 13:48:11 +0100

    handle `readFile()` being given a directory

--- src/readFile.ts
+++ src/readFile.ts
@@ -31,7 +31,10 @@ async function readFile(
 
     return content;
   } catch (error) {
-    if (throwNotFound === false && error.code === 'ENOENT') {
+    if (
+      throwNotFound === false &&
+      (error.code === 'ENOENT' || error.code === 'EISDIR')
+    ) {
       return null;
     }
 
@@ -47,7 +50,10 @@ function readFileSync(filepath: string, options: Options = {}): string | null {
 
     return content;
   } catch (error) {
-    if (throwNotFound === false && error.code === 'ENOENT') {
+    if (
+      throwNotFound === false &&
+      (error.code === 'ENOENT' || error.code === 'EISDIR')
+    ) {
       return null;
     }
 
