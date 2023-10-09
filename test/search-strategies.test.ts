5cd0840a47790a777c56ae90f1c26373219eb7b4
Daniel Fischer <daniel@d-fischer.dev>
2023-10-09 22:13:07 +0200

    chore: add linux-only test for XDG basedir

--- test/search-strategies.test.ts
+++ test/search-strategies.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
 import { cosmiconfig, cosmiconfigSync, Options, OptionsSync } from '../src';
 import { TempDir } from './util';
 
@@ -39,37 +39,79 @@ describe('search strategies', () => {
   });
 
   describe('global', () => {
-    beforeEach(() => {
-      temp.clean();
-      temp.createDir('a/b');
-      temp.createFile('a/.barrc.yml', 'result: 4');
-      temp.createFile('.barrc.yml', 'result: 5');
-    });
-
-    const startDir = temp.absolutePath('a/b');
-    const explorerOptions = {
-      searchStrategy: 'global',
-      stopDir: temp.absolutePath('.'),
-    } satisfies Partial<Options | OptionsSync>;
-
-    const checkResult = (result: any) => {
-      expect(result).toEqual({
-        config: { result: 4 },
-        filepath: temp.absolutePath('a/.barrc.yml'),
+    describe('finds config in parent directories', () => {
+      beforeEach(() => {
+        temp.clean();
+        temp.createDir('a/b');
+        temp.createFile('a/.barrc.yml', 'result: 4');
+        temp.createFile('.barrc.yml', 'result: 5');
       });
-    };
 
-    test('async', async () => {
-      const explorer = cosmiconfig('bar', explorerOptions);
-      const result = await explorer.search(startDir);
-      checkResult(result);
+      const startDir = temp.absolutePath('a/b');
+      const explorerOptions = {
+        searchStrategy: 'global',
+        stopDir: temp.absolutePath('.'),
+      } satisfies Partial<Options | OptionsSync>;
+
+      const checkResult = (result: any) => {
+        expect(result).toEqual({
+          config: { result: 4 },
+          filepath: temp.absolutePath('a/.barrc.yml'),
+        });
+      };
+
+      test('async', async () => {
+        const explorer = cosmiconfig('bar', explorerOptions);
+        const result = await explorer.search(startDir);
+        checkResult(result);
+      });
+
+      test('sync', () => {
+        const explorer = cosmiconfigSync('bar', explorerOptions);
+        const result = explorer.search(startDir);
+        checkResult(result);
+      });
     });
 
-    test('sync', () => {
-      const explorer = cosmiconfigSync('bar', explorerOptions);
-      const result = explorer.search(startDir);
-      checkResult(result);
-    });
+    if (process.platform === 'linux') {
+      describe('finds config in OS default directory (XDG)', () => {
+        const origConfigFolder = process.env.XDG_CONFIG_HOME;
+        beforeEach(() => {
+          temp.clean();
+          temp.createDir('a/b');
+          temp.createFile('config/foo/config.yml', 'result: 6');
+          process.env.XDG_CONFIG_HOME = temp.absolutePath('config');
+        });
+        afterEach(() => {
+          process.env.XDG_CONFIG_HOME = origConfigFolder;
+        });
+
+        const startDir = temp.absolutePath('a/b');
+        const explorerOptions = {
+          searchStrategy: 'global',
+          stopDir: temp.absolutePath('.'),
+        } satisfies Partial<Options | OptionsSync>;
+
+        const checkResult = (result: any) => {
+          expect(result).toEqual({
+            config: { result: 6 },
+            filepath: temp.absolutePath('config/foo/config.yml'),
+          });
+        };
+
+        test('async', async () => {
+          const explorer = cosmiconfig('foo', explorerOptions);
+          const result = await explorer.search(startDir);
+          checkResult(result);
+        });
+
+        test('sync', () => {
+          const explorer = cosmiconfigSync('foo', explorerOptions);
+          const result = explorer.search(startDir);
+          checkResult(result);
+        });
+      });
+    }
   });
 
   describe('project', () => {
